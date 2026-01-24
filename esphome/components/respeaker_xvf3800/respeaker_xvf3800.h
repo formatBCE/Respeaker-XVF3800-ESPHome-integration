14b93a5ad6de290b442ae075b8b5a26d25c5dcb9
fitoori <sbajaj@me.com>
2026-01-23 22:55:02 -0500

    Remove unused mute state binary sensor

--- esphome/components/respeaker_xvf3800/respeaker_xvf3800.h
+++ esphome/components/respeaker_xvf3800/respeaker_xvf3800.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "esphome/components/binary_sensor/binary_sensor.h"
 #include "esphome/components/text_sensor/text_sensor.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/i2c/i2c.h"
@@ -195,10 +194,6 @@ class RespeakerXVF3800 : public i2c::I2CDevice, public Component {
     this->firmware_bin_length_ = len;
   }
 
-  void set_mute_state(binary_sensor::BinarySensor* mute_state) {
-    this->mute_state_ = mute_state;
-  }
-
   void set_firmware_version(text_sensor::TextSensor* firmware_version) {
     this->firmware_version_ = firmware_version;
   }
@@ -249,7 +244,6 @@ class RespeakerXVF3800 : public i2c::I2CDevice, public Component {
   bool dfu_check_if_ready_();
 
   GPIOPin *reset_pin_{nullptr};
-  binary_sensor::BinarySensor *mute_state_{nullptr};
   text_sensor::TextSensor *firmware_version_{nullptr};
 
   bool get_firmware_version_();
